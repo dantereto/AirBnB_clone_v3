ca406a1dcacdad633ed5788d22c2687519a0960b
dantereto <2247@holbertonschool.com>
2021-09-23 23:18:13 +0000

    try to make task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', function () { 
+    list_n = []
+    list_id = []
+    $('input:checkbox').click(function () {
+	if ($this.is(':checked')) {
+	    list_id.push($this.attr('data-id'));
+	    list_n.push($this.attr('data-name'));
+	} else {
+	  list_id = $.grep(list_id, function (data) {
+	      return data != $this.attr('data-id');
+	  });  
+	  list_n = $.grep(list_n, function (data) {
+	      return data != $this.attr('data-name');
+	  });
+	}
+	const update = list_n.join(', ');
+	$('.amenities h4'.text(update);
+});
+});
