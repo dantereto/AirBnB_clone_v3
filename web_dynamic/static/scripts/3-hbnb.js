b769f19cc33461864fe58adf47e516e8a4428190
ForeroAlexander <foreroalex26@gmail.com>
2021-09-27 02:39:43 -0500

    View Places

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', function () {
+	const HOST = '127.0.0.1'; 
 	const amenity_n = {};
 	$('input[type=checkbox]').change(function () {
 		if ($(this).prop('checked')) {
@@ -14,7 +15,7 @@ window.addEventListener('load', function () {
 	});
 	$.ajax({
 		type: 'GET',
-		url: 'http://0.0.0.0:5001/api/v1/status/',
+		url: `http://${HOST}:5001/api/v1/status/`,
 		dataType: 'json',
 		success: function (data) {
 			if (data.status === 'OK') {
@@ -25,16 +26,15 @@ window.addEventListener('load', function () {
 		}
 	});
 	$.ajax({
-		url: 'http://0.0.0.0:5001/api/v1/places_search',
+		url: `http://${HOST}:5001/api/v1/places_search`,
 		dataType: 'json',
 		type: 'post',
 		contentType: 'application/json',
 		data: JSON.stringify({}),
 		success: function (data) {
-			for (const i = 0; i < data.lenght; i++) {
-				const place = data[i]
-				$('places ').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + '</div><div class="number_rooms">' + place.number_rooms + '</div><div class="number_bathrooms">' + place.number_bathrooms + '</div></div><div class="description">' + place.description + '</div></article>');
-			}
+			data.forEach(place => 
+				$('.places').append($('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + '</div><div class="number_rooms">' + place.number_rooms + '</div><div class="number_bathrooms">' + place.number_bathrooms + '</div></div><div class="description">' + place.description + '</div></article>'))
+			);
 		}
 	});
 });
